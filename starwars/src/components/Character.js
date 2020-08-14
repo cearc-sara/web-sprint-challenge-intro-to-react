decab110de8d1589863ce5290e63e2282b554e6c
Sara Cearc <cearc-sara@lambdastudents.com>
2020-08-14 13:00:06 -0400

    working on app

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,22 @@
 // Write your Character component here
+import React from 'react';
+import axios from 'axios'
+import styled from 'styled-components'
+
+const Character = (props) => {
+    const {characters} = props;
+    
+
+
+
+
+    return (
+        <div>
+           <p>{characters.map(character => {
+               return console.log(character)
+           })}</p>
+        </div>
+    )
+};
+
+export default Character;
---
+++ starwars/src/theme/index.js
@@ -0,0 +1,17 @@
+const theme = {
+    primaryColor: 'cyan',
+    secondaryColor: 'purple',
+    tertiaryColor: 'teal',
+    white: 'white',
+    black: 'black',
+    fontSize: '1.2rem',
+    paddingSmall: '0.5em',
+    paddingMedium: '1.5em',
+    margins: {
+        big: '3em',
+        medium: '1.5em',
+        small: '0.5em',
+    }
+}
+
+export default theme
